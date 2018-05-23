dfbead16cf42e77003bc6373a1a6ed96c9fd998f
Vladimir Evseev <ve@elonsoft.ru>
2018-05-23 22:21:54 +0300

    Improve code style

--- autosize.directive.ts
+++ autosize.directive.ts
@@ -1,28 +1,29 @@
-import {AfterViewInit, Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: 'textarea[autosize]'
 })
 export class AutosizeDirective implements AfterViewInit {
-  @HostBinding('style.overflow') overflow = 'hidden';
-  @HostBinding('rows') rows = 1;
+  @HostBinding('style.overflow')
+  public overflow = 'hidden';
+
+  @HostBinding('rows')
+  public rows = 1;
+
+  constructor(public elem: ElementRef) { }
+
+  public ngAfterViewInit() {
+    this.resize();
+  }
 
   @HostListener('input')
   public onInput() {
     this.resize();
   }
 
-  constructor(public hostElem: ElementRef) {
-  }
-
-  ngAfterViewInit(): void {
-    this.resize();
-  }
-
   public resize() {
-    const style = this.hostElem.nativeElement.style;
+    const { style, scrollHeight } = this.elem.nativeElement;
     style.height = 'auto';
-    style.height = `${this.hostElem.nativeElement.scrollHeight}px`;
+    style.height = `${scrollHeight}px`;
   }
-
 }
